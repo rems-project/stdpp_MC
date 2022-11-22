2f0b1895c13e535f85fafaa003e0b2f4a410e9f5
Ricardo Almeida <3705908+ric-almeida@users.noreply.github.com>
2022-11-22 19:16:44 +0000

    Adding files for the first time

---
+++ stdpp/prelude.v
@@ -0,0 +1,15 @@
+From stdpp Require Export
+  BVbase
+  tactics
+  orders
+  BVoption
+  vector
+  BVnumbers
+  relations
+  sets
+  fin_sets
+  listset
+  list
+  list_numbers
+  lexico.
+From stdpp Require Import options.
